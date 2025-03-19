04006f8fa580935798e8202a3037841b98b7ebd5
IZYL CLEMENA <clemenaizyl@gmail.com>
2025-03-19 13:42:49 +0800

    izyl changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,12 @@
 <script></script>
 
-<nav class="w-full h-16 p-2 bg-white border-b-4 border-b-yellow-300 flex">
-  <img
-    src="https://www.treasury.gov.ph/wp-content/uploads/2016/08/cropped-BTr-Original-Logo-1-1.png"
-    alt=""
-  />
+<nav class="w-full h-16 p-2 bg-white border-b-4 border-b-yellow-300 flex fix">
+  <img src="images/BTR-logo.png" alt="" />
   <h1 class=" items-center flex font-bold m-2">
     BUREAU OF THE TREASURY - Region V
   </h1>
 
-  <div class="ml-auto mr-0 border justify-end flex">
+  <div class=" w-full ml-auto mr-0 border flex">
     <ul class="flex gap-5 justify-end p-2 pr-5">
       <li>Home</li>
       <li>About Us</li>
@@ -22,6 +19,40 @@
 </nav>
 
 <div
-  style="width: 100%; z-index: -1; background-image: url('images/BTRV-Building.jpg'); background-size: cover;"
-  class="h-screen blur-xs"
-></div>
+  style="width: 100%; z-index: -1; background-image: url('images/BTR-Building2.png'); background-size: cover;"
+  class="h-screen"
+>
+  
+  
+</div>
+
+<div
+  style="width: 100%; z-index: -1; background-image: url('images/BTr-Treasury.jpg'); background-size: cover;"
+  class="h-screen flex justify-center items-center"
+>
+
+<div class="w-200 h-100">
+
+    <img src="images/BTr-Treasury.jpg" alt="">
+</div>
+  
+  
+</div>
+
+
+
+<footer class="bg-white">
+    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div class="md:flex md:justify-between">
+          <div class="mb-6 md:mb-0">
+              <a href="#" class="flex items-center">
+                  <img src="images/BagongPilipinas-logo.png" class="h-13 me-3" alt="" />
+                  <img src="images/DOF-logo.png" class="h-13 me-3" alt="">
+                  <img src="images/BTR-logo.png" class="h-13 me-3" alt="">
+                  <span class="self-center text-3xl font-semibold whitespace-nowrap">Bureau of the Treasury</span>
+              </a>
+          </div>
+         
+          </div>
+      </div>
+</footer>
