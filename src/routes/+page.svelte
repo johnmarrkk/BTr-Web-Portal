7a320f9a596339eb99fce84eee28da4108f8ec83
IZYL CLEMENA <clemenaizyl@gmail.com>
2025-03-19 11:05:15 +0800

    add images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,27 @@
 <script></script>
 
-<nav class="w-full h-18 bg-blue-700 flex">
-    <h1 class="">hello</h1>
+<nav class="w-full h-16 p-2 bg-white border-b-4 border-b-yellow-300 flex">
+  <img
+    src="https://www.treasury.gov.ph/wp-content/uploads/2016/08/cropped-BTr-Original-Logo-1-1.png"
+    alt=""
+  />
+  <h1 class=" items-center flex font-bold m-2">
+    BUREAU OF THE TREASURY - Region V
+  </h1>
+
+  <div class="ml-auto mr-0 border justify-end flex">
+    <ul class="flex gap-5 justify-end p-2 pr-5">
+      <li>Home</li>
+      <li>About Us</li>
+      <li>Contact Us</li>
+    </ul>
+  </div>
+  <div class="ml-auto mr-2 border flex items-center">
+    <h1 class="font-bold">LOGIN</h1>
+  </div>
 </nav>
+
+<div
+  style="width: 100%; z-index: -1; background-image: url('images/BTRV-Building.jpg'); background-size: cover;"
+  class="h-screen blur-xs"
+></div>
