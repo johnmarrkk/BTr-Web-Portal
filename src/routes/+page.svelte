e51f3a9a7a4834a9974f006401001fbd1dd11578
IZYL CLEMENA <clemenaizyl@gmail.com>
2025-03-20 13:17:27 +0800

    change new me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,21 @@
 <script></script>
 
 <nav class="w-full h-16 p-2 bg-white border-b-4 border-b-yellow-300 flex fix">
-  <img src="images/BTR-logo.png" alt="" />
-  <h1 class=" items-center flex font-bold m-2">
+  <img src="images/BTR-logo.png" class="h-12" alt="" />
+  <h1 class=" items-center flex w-full font-bold m-2">
     BUREAU OF THE TREASURY - Region V
   </h1>
 
-  <div class=" w-full ml-auto mr-0 border flex">
-    <ul class="flex gap-5 justify-end p-2 pr-5">
-      <li>Home</li>
-      <li>About Us</li>
-      <li>Contact Us</li>
+  <div class="w-full flex mr-4">
+    <ul class="flex gap-10 items-center p-2 pr-5 ml-auto justify-between">
+      <li class="hover:bg-amber-200 cursor-pointer p-3">Home</li>
+      <li class="hover:bg-amber-200 cursor-pointer p-3">About Us</li>
+      <li class="hover:bg-amber-200 cursor-pointer p-3">Contact Us</li>
     </ul>
   </div>
-  <div class="ml-auto mr-2 border flex items-center">
-    <h1 class="font-bold">LOGIN</h1>
+
+  <div class="ml-auto mr-2 w-40 flex items-center">
+    <h1 class="font-bold hover:bg-amber-200 cursor-pointer p-3">Log in</h1>
   </div>
 </nav>
 
@@ -22,37 +23,49 @@
   style="width: 100%; z-index: -1; background-image: url('images/BTR-Building2.png'); background-size: cover;"
   class="h-screen"
 >
-  
-  
+  <div>
+    <h1
+      class="text-6xl font-serif text-white font-extralight pt-20 pl-20 w-1/2 whitespace-pre-wrap"
+    >
+      Makabago, Maunlad at Mabisang Pamamahala ng Ingatang-yaman
+    </h1>
+
+    <button class="ml-100 p-3 bg-white rounded-lg mt-50">Get Started</button>
+  </div>
 </div>
 
 <div
   style="width: 100%; z-index: -1; background-image: url('images/BTr-Treasury.jpg'); background-size: cover;"
-  class="h-screen flex justify-center items-center"
+  class="h-screen flex justify-center"
+  
 >
+  <div class="w-200 h-100 mt-25">
 
-<div class="w-200 h-100">
+    <button class="px-4 p-2 bg-amber-200 rounded-lg mt-5 mb-2 font-bold">FEATURED VIDEO</button>
+    <!-- svelte-ignore a11y_media_has_caption -->
 
-    <img src="images/BTr-Treasury.jpg" alt="">
+    <video width="800" height="450" autoplay muted loop controls class="rounded-2xl"> 
+        <source src="/images/11_FinLit _v7_.mp4_en.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+      
+      
+  </div>
 </div>
-  
-  
-</div>
-
-
 
 <footer class="bg-white">
-    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div class="md:flex md:justify-between">
-          <div class="mb-6 md:mb-0">
-              <a href="#" class="flex items-center">
-                  <img src="images/BagongPilipinas-logo.png" class="h-13 me-3" alt="" />
-                  <img src="images/DOF-logo.png" class="h-13 me-3" alt="">
-                  <img src="images/BTR-logo.png" class="h-13 me-3" alt="">
-                  <span class="self-center text-3xl font-semibold whitespace-nowrap">Bureau of the Treasury</span>
-              </a>
-          </div>
-         
-          </div>
+  <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+    <div class="md:flex md:justify-between">
+      <div class="mb-6 md:mb-0">
+        <a href="#" class="flex items-center">
+          <img src="images/BagongPilipinas-logo.png" class="h-13 me-3" alt="" />
+          <img src="images/DOF-logo.png" class="h-13 me-3" alt="" />
+          <img src="images/BTR-logo.png" class="h-13 me-3" alt="" />
+          <span class="self-center text-3xl font-semibold whitespace-nowrap"
+            >Bureau of the Treasury</span
+          >
+        </a>
       </div>
+    </div>
+  </div>
 </footer>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,60 @@
+<script></script>
+
+<nav class="bg-blue-900 h-16 flex fixed w-full  ">
+ 
+    
+</nav>
+
+<div class="grid grid-cols-4 h-screen">
+    
+    <div class=" bg-gray-100 p-5 border-1 border-gray-400 flex flex-col items-center h-screen">
+        <div>
+             <img src="images/BTR-logo.png" class="h-40 mt-30 mb-10" alt="" />
+        </div>
+       
+
+        <div class="relative text-black border-s border-gray-400 bg-gray-10">
+
+            <ul>
+                 <li class="mb-15 ms-6">            
+                <span class="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -start-4 ring-4 ring-white dark:ring-gray-100 dark:bg-blue-500">
+                    <svg class="w-3.5 h-3.5 text-blue-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+                    </svg>
+                </span>
+                <h3 class="font-medium leading-tight">Information</h3>
+            </li>
+            
+            <li class="mb-15 ms-6">            
+                <span class="absolute flex items-center justify-center w-8 h-8 bg-blue-200 rounded-full -start-4 ring-4 ring-white dark:ring-gray-100 dark:bg-gray-300">
+                   <h1 class="text-black font-semibold">2</h1>
+                </span>
+                <h3 class="font-medium leading-tight">Organization</h3>
+            </li>
+
+            <li class="mb-15 ms-6">            
+                <span class="absolute flex items-center justify-center w-8 h-8 bg-blue-200 rounded-full -start-4 ring-4 ring-white dark:ring-gray-100 dark:bg-gray-300">
+                    <h1 class="text-black font-semibold">3</h1>
+                </span>
+                <h3 class="font-medium leading-tight">Password</h3>
+            </li>
+
+            <li class=" ms-6">            
+                <span class="absolute flex items-center justify-center w-8 h-8 bg-blue-200 rounded-full -start-4 ring-4 ring-white dark:ring-gray-100 dark:bg-gray-300">
+                    <h1 class="text-black font-semibold">4</h1>
+                </span>
+                <h3 class="font-medium leading-tight">Review</h3>
+            </li>
+
+            </ul>
+           
+
+
+        </div>
+
+    </div>
+  
+    <div class="bg-white p-5">
+    </div>
+  </div>
+  
