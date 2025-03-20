3983e83d574c729e2c7af96fdf7ccf4662e1f3b6
IZYL CLEMENA <clemenaizyl@gmail.com>
2025-03-20 14:37:50 +0800

    Create Account-Information

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -7,7 +7,7 @@
 
 <div class="grid grid-cols-4 h-screen">
     
-    <div class=" bg-gray-100 p-5 border-1 border-gray-400 flex flex-col items-center h-screen">
+    <div class=" bg-gray-100 p-20 border-1 border-gray-400 flex flex-col items-center h-screen">
         <div>
              <img src="images/BTR-logo.png" class="h-40 mt-30 mb-10" alt="" />
         </div>
@@ -47,14 +47,69 @@
             </li>
 
             </ul>
-           
-
-
         </div>
-
     </div>
   
     <div class="bg-white p-5">
+        <div class="w-full pt-5 flex flex-col items-center justify-center px-130 py-8 mx-auto md:h-screen lg:py-0">
+              
+            <div class="w-100 h-150  bg-white rounded-lg shadow dark:border md:mt-0  dark:bg-white dark:border-gray-300">
+
+                <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 class="mt-5 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Create Account</h2>
+                  </div>
+
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                   <form class="space-y-4 md:space-y-6" action="#">
+
+                    <div class="relative">
+                        <input type="email" id="email" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" />
+                        <label for="email" class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Email</label>
+                    </div>
+
+                    <div class="relative">
+                        <input type="first_name" id="first_name" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" />
+                        <label for="first_name" class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">First Name</label>
+                    </div>
+
+                    <div class="relative">
+                        <input type="middle_name" id="middle_name" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" />
+                        <label for="middle_name" class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Middle Name</label>
+                    </div>
+
+                    <div class="relative">
+                        <input type="last_name" id="last_name" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" />
+                        <label for="last_name" class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Last Name</label>
+                    </div>
+
+                    <div class="relative">
+                        <label for="sex" class="block text-sm text-gray-500">Sex</label>
+                        <select id="sex" class="block w-full px-2.5 py-3 text-sm text-gray-900 border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-1 focus:ring-blue-600 focus:border-blue-600">
+                            <option value="" disabled selected>- select -</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                    </div>
+
+                    <div class="relative">
+                        <input type="mobile_number" id="mobile_number" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" />
+                        <label for="mobile_number" class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Mobile Number</label>
+                    </div>
+                    
+                    <div class="flex space-x-4">
+                        <button type="button" class="w-1/2 text-black bg-gray-300 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-gray-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                            Cancel
+                        </button>
+                        <button type="button" class="w-1/2 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                            Next
+                        </button>
+                    </div>
+                    
+
+                   </form>
+                </div>
+            </div>
+          </div>
     </div>
   </div>
   
