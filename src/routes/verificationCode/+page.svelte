5aad6e49e2733230b873cd4bcbd662a10fde3ba8
MARK JOE POGI <bidarjoe0004@gmail.com>
2025-03-25 09:48:58 +0800

    Joe Changes

---
+++ src/routes/verificationCode/+page.svelte
@@ -0,0 +1,76 @@
+<script>
+
+</script>
+
+
+<nav class="bg-blue-900 h-16 flex fixed w-full items-center">
+    <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="images/BTR_logo.png" class="h-11 m-3" alt="BTR Logo" />
+    </a>
+    <div class="flex flex-col leading-tight">
+      <span
+        class="self-center text-xl font- inter whitespace-nowrap dark:text-white"
+        >BUREAU OF THE TREASURY</span
+      >
+      <span class=" text-xs font-semibold whitespace-nowrap dark:text-white">
+        REGIONAL OFFICE V</span
+      >
+    </div>
+  
+    <div class="flex items-center mr-3 ml-auto space-x-4">
+      <img src="images/DOF-logo.png" class="h-11" alt="DOF Logo" />
+      <img src="images/BagongPilipinas-logo.png" class="h-11" alt="Bagong Pilipinas Logo" />
+    </div>
+  </nav>
+
+ 
+<div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <div class="max-w-sm p-6">
+    
+        <img src="images/password.png" class= " mb-3 mx-auto h-20"alt="password icon">
+        <h5 class=" text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">Verification Code</h5>
+      
+        <p class=" text-center mb-3 font-Inter text-gray-700 dark:text-black-800">We have sent a verification code to your email johnmarkk03@gmail.com</p>
+
+        
+        <form class="max-w-sm mx-auto">
+          <div class=" ml-10 flex mb-2 space-x-2 rtl:space-x-reverse">
+              <div>
+                  <label for="code-1" class="sr-only">First code</label>
+                  <input type="text" maxlength="1" data-focus-input-init data-focus-input-next="code-2" id="code-1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" required />
+              </div>
+              <div>
+                  <label for="code-2" class="sr-only">Second code</label>
+                  <input type="text" maxlength="1" data-focus-input-init data-focus-input-prev="code-1" data-focus-input-next="code-3" id="code-2" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" required />
+              </div>
+              <div>
+                  <label for="code-3" class="sr-only">Third code</label>
+                  <input type="text" maxlength="1" data-focus-input-init data-focus-input-prev="code-2" data-focus-input-next="code-4" id="code-3" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" required />
+              </div>
+              <div>
+                  <label for="code-4" class="sr-only">Fourth code</label>
+                  <input type="text" maxlength="1" data-focus-input-init data-focus-input-prev="code-3" data-focus-input-next="code-5" id="code-4" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" required />
+              </div>
+              <div>
+                  <label for="code-5" class="sr-only">Fifth code</label>
+                  <input type="text" maxlength="1" data-focus-input-init data-focus-input-prev="code-4" data-focus-input-next="code-6" id="code-5" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" required />
+              </div>
+              <div>
+                  <label for="code-6" class="sr-only">Sixth code</label>
+                  <input type="text" maxlength="1" data-focus-input-init data-focus-input-prev="code-5" id="code-6" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" required />
+              </div>
+          </div>
+        </form>
+
+        
+
+        <button type="button" class=" w-full text-white bg-blue-900 hover:bg-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-500 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Verify and Submit</button>
+
+        <p class=" text-center mt-4 font-bold font-Inter text-gray-400 dark:text-black-800">RESEND CODE</p>
+
+        
+     </div>
+</div>
+
+
+
