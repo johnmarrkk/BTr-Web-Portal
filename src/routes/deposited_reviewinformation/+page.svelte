f1de4bb00ba747b31fd6801b07fd96d2d954448f
IZYL CLEMENA <clemenaizyl@gmail.com>
2025-03-25 09:08:58 +0800

    m25-izyl

---
+++ src/routes/deposited_reviewinformation/+page.svelte
@@ -0,0 +1,81 @@
+<script>
+    // @ts-nocheck
+    </script>
+    
+    <nav class="bg-blue-900 h-16 flex fixed w-full items-center">
+      <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="images/BTR_logo.png" class="h-11 m-3" alt="BTR Logo" />
+      </a>
+      <div class="flex flex-col leading-tight">
+        <span
+          class="self-center text-xl font- inter whitespace-nowrap dark:text-white"
+          >BUREAU OF THE TREASURY</span
+        >
+        <span class=" text-xs font-semibold whitespace-nowrap dark:text-white">
+          REGIONAL OFFICE V</span
+        >
+      </div>
+    
+      <div class="flex items-center mr-3 ml-auto space-x-4">
+        <img src="images/DOF-logo.png" class="h-11" alt="DOF Logo" />
+        <img src="images/BagongPilipinas-logo.png" class="h-11" alt="Bagong Pilipinas Logo" />
+      </div>
+    </nav>
+
+    <div class="w-full pt-5 flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div class="w-200 h-120  bg-white rounded-lg shadow dark:border md:mt-0  dark:bg-white dark:border-gray-300">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                
+                <form class="space-y-1 md:space-y-3" action="#">
+
+                    <h2 class="text-base font-bold">Request for Deposited National Collections</h2>
+                    <h2 class="text-sm font-semibold">Review Information</h2>
+                    
+                    <div class="border border-gray-400 w-full rounded-md">
+                        <table class="w-full border-collapse border border-gray-400">
+                            <tbody>
+                                <tr class="border border-gray-400">
+                                    <td class="text-sm p-2 font-semibold border border-gray-400">Type of Request:</td>
+                                    <td class="text-sm p-2 border rounded-md     border-gray-400">NCO</td>
+                                </tr>
+                                <tr class="border border-gray-400">
+                                    <td class="text-sm p-2 font-semibold border border-gray-400">U A C S Code:</td>
+                                    <td class="text-sm p-2 border border-gray-400">000000000000000000001</td>
+                                </tr>
+                                <tr class="border border-gray-400">
+                                    <td class="text-sm p-2 font-semibold border border-gray-400">Agency:</td>
+                                    <td class="text-sm p-2 border border-gray-400">Bonga National High School</td>
+                                </tr>
+                                <tr class="border border-gray-400">
+                                    <td class="text-sm p-2 font-semibold border border-gray-400">Name of N C O:</td>
+                                    <td class="text-sm p-2 border border-gray-400">KENTH SIBAY</td>
+                                </tr>
+                                <tr class="border border-gray-400">
+                                    <td class="text-sm p-2 font-semibold border border-gray-400">N C O Code:</td>
+                                    <td class="text-sm p-2 border border-gray-400">000000000002</td>
+                                </tr>
+                                <tr class="border border-gray-400">
+                                    <td class="text-sm p-2 font-semibold border border-gray-400">Date Covered of The Collections:</td>
+                                    <td class="text-sm p-2 border border-gray-400">2025-02-04 to 2025-02-05</td>
+                                </tr>
+                                <tr class="border border-gray-400">
+                                    <td class="text-sm p-2 font-semibold border border-gray-400">Total Amount Requested:</td>
+                                    <td class="text-sm p-2 border border-gray-400"></td>
+                                </tr>
+                                <tr class="border border-gray-400">
+                                    <td class="text-sm p-2 font-semibold border border-gray-400">Uploaded Attachments:</td>
+                                    <td class="p-2 border border-gray-400"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>  
+                    
+                    <div class="flex justify-end space-x-2 p-3">
+                        <button class="text-sm px-5 py-1.5 bg-gray-300 text-black rounded-lg">Previous</button>
+                        <button class="text-sm px-5 py-1.5 bg-blue-600 text-white rounded-lg">Submit</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+      </div>
