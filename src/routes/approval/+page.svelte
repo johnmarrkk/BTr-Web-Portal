7d3e70e24815fb4a3d74da1e1ee63531c3370c9d
Kenth Alfie Sibay <kenthsibay3@gmail.com>
2025-03-25 09:43:24 +0800

    m25-kenth

---
+++ src/routes/approval/+page.svelte
@@ -0,0 +1,46 @@
+<script></script>
+
+<nav class="bg-blue-900 h-16 flex fixed w-full items-center">
+    <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="images/BTR_logo.png" class="h-11 m-3" alt="BTR Logo" />
+    </a>
+    <div class="flex flex-col leading-tight">
+      <span
+        class="self-center text-xl font- inter whitespace-nowrap text-white"
+        >BUREAU OF THE TREASURY</span
+      >
+      <span class=" text-xs font-semibold whitespace-nowrap text-white">
+        REGIONAL OFFICE V</span
+      >
+    </div>
+  
+    <div class="flex items-center mr-3 ml-auto space-x-4">
+      <img src="images/DOF-logo.png" class="h-11" alt="DOF Logo" />
+      <img src="images/BagongPilipinas-logo.png" class="h-11" alt="Bagong Pilipinas Logo" />
+    </div>
+  </nav>
+
+
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="w-165 h-90 p-8 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 text-center flex flex-col items-center">
+        <img src="images/Btr_Banner.png" alt="Approval Pending" class="w-100 h-24 mb-4">
+
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Your account is under approval.
+        </h5>
+
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">
+            Your account creation request is currently under review and verification by the Bureau of the Treasury personnel. Once your account is approved, an email confirmation will be sent for further instructions.
+        </p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">
+            Thank you for your patience and understanding!
+        </p>
+
+        <a href="#" class="inline-flex items-center justify-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-32 mt-4">
+            Done
+        </a>
+    </div>
+</div>
+
+
+
