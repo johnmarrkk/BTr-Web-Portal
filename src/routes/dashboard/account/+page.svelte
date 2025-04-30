2b43c41e4a2d188f7f631b47184fe9e794da8778
IZYL CLEMENA <clemenaizyl@gmail.com>
2025-04-30 13:51:36 +0800

    account

--- src/routes/dashboard/account/+page.svelte
+++ src/routes/dashboard/account/+page.svelte
@@ -1,5 +1,5 @@
-<div class="relative bg-blue-200 bg-cover bg-center h-60 overflow-hidden" style="background-image: url('https://images.template.net/96416/blue-watercolor-background-d6g5y.jpg');">
-    <button class="absolute top-45 right-4 bg-blue-600 text-white px-4 py-2 text-sm rounded hover:bg-blue-700">
+<div class="relative bg-blue-200 bg-cover bg-center h-60 overflow-hidden" style="background-image: url('https://www.myfreetextures.com/wp-content/uploads/2014/11/blue-abstract-background-texture-900x589.jpg?ezimgfmt=ng%3Awebp%2Fngcb1%2Frs%3Adevice%2Frscb1-1');">
+    <button class="absolute top-45 right-4 bg-white text-blue-600 px-4 py-2 text-sm rounded hover:bg-blue-200">
       ✎ Edit profile
     </button>
     <div class="flex flex-col items-center mt-10">
@@ -8,3 +8,59 @@
       <p class="text-white text-sm">Office Head<br>Client</p>
     </div>
   </div>
+
+  <!-- Personal Info Section -->
+<div class="bg-gray-50 rounded-lg shadow-md p-6 max-w-7xl mx-auto mt-3">
+  <h2 class="text-2xl font-bold mb-4">Personal Info</h2>
+  <div class="divide-y divide-gray-300">
+    
+    <div class="flex items-start gap-x-4 py-2">
+      <span class="font-semibold">Full name:</span>
+      <span>IZYL BUISING CLEMEÑA</span>
+    </div>
+    
+    <div class="flex items-start gap-x-4 py-2">
+      <span class="font-semibold">Mobile:</span>
+      <span>09153856038</span>
+    </div>
+    
+    <div class="flex items-start gap-x-4 py-2">
+      <span class="font-semibold">Email:</span>
+      <span>clemenaizyl@gmail.com</span>
+    </div>
+    
+    <div class="flex items-start gap-x-4 py-2">
+      <span class="font-semibold">Client Type:</span>
+      <span>LGU</span>
+    </div>
+    
+    <div class="flex items-start gap-x-4 py-2">
+      <span class="font-semibold">Position:</span>
+      <span>Office Head</span>
+    </div>
+    
+    <div class="flex items-start gap-x-4 py-2">
+      <span class="font-semibold">Agency:</span>
+      <span>Albay 3rd District Engineering Office</span>
+    </div>
+    
+    <div class="flex items-start gap-x-4 py-2">
+      <span class="font-semibold">Department:</span>
+      <span>Department of Public Works and Highways (DPWH)</span>
+    </div>
+    
+    <div class="flex items-start gap-x-4 py-2">
+      <span class="font-semibold">Office Address:</span>
+      <span>Legazpi Albay</span>
+    </div>
+    
+    <div class="flex items-start gap-x-4 py-2">
+      <span class="font-semibold">Date Account Created:</span>
+      <span>December 4, 2024</span>
+    </div>
+    
+  </div>
+</div>
+
+
+  
